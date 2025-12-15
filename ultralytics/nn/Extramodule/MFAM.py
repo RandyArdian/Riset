07fa5fd6dfbdfc82f1854ab35aed4519e1ebea0b
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:30:13 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/Extramodule/MFAM.py
+++ ultralytics/nn/Extramodule/MFAM.py
@@ -1,5 +1,5 @@
-import torch
 import math
+
 import torch.nn as nn
 
 
@@ -11,17 +11,18 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
 
+
 class Conv(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
-    #default_act = nn.SiLU()  # default activation
+    # default_act = nn.SiLU()  # default activation
 
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
-        self.act =  nn.SiLU() if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.act = nn.SiLU() if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
@@ -37,34 +38,33 @@ class DWConv(Conv):
 
     def __init__(self, c1, c2, k=1, s=1, d=1, act=True):  # ch_in, ch_out, kernel, stride, dilation, activation
         """Initialize Depth-wise convolution with given parameters."""
-        super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), d=d, act=act)  #gcd mencari FPB dari dua bilangan
+        super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), d=d, act=act)  # gcd mencari FPB dari dua bilangan
+
 
 class MFAM(nn.Module):
     def __init__(self, c1, c2, expand_ratio=1):
-        super(MFAM, self).__init__()
-        hidden_dim= c1 * expand_ratio
+        super().__init__()
+        hidden_dim = c1 * expand_ratio
         # DWconv pertama
         self.cv1 = DWConv(c1, hidden_dim, k=3)
         # Dwconv kedua
         self.cv2 = DWConv(c1, hidden_dim, k=5)
         # path 3
-        self.cv3= DWConv(c1, hidden_dim,k=(1,7))
-        self.cv4= DWConv(hidden_dim, hidden_dim,k=(7,1))
+        self.cv3 = DWConv(c1, hidden_dim, k=(1, 7))
+        self.cv4 = DWConv(hidden_dim, hidden_dim, k=(7, 1))
 
         # path 4
-        self.cv5= DWConv(c1, hidden_dim,k=(1,9))
-        self.cv6= DWConv(hidden_dim, hidden_dim,k=(9,1))
-        
-        # Konvolusi terakhir
-        self.cv7= Conv(hidden_dim, c2, k=1)
+        self.cv5 = DWConv(c1, hidden_dim, k=(1, 9))
+        self.cv6 = DWConv(hidden_dim, hidden_dim, k=(9, 1))
 
+        # Konvolusi terakhir
+        self.cv7 = Conv(hidden_dim, c2, k=1)
 
     def forward(self, x):
-        identity=x
-        path1= self.cv1(x)
-        path2= self.cv2(x)
-        path3= self.cv4(self.cv3(x))
-        path4= self.cv6(self.cv5(x))
-        add=path1+path2+path3+path4+identity
+        identity = x
+        path1 = self.cv1(x)
+        path2 = self.cv2(x)
+        path3 = self.cv4(self.cv3(x))
+        path4 = self.cv6(self.cv5(x))
+        add = path1 + path2 + path3 + path4 + identity
         return self.cv7(add)
-
