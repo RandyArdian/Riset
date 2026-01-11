a72291872edf0acab39da4905ba4e4cb907b50f0
RandyArdian <112707872+RandyArdian@users.noreply.github.com>
2026-01-11 12:14:53 +0800

    Fix Conv layer padding in head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -382,10 +382,10 @@ class Detect(nn.Module):
         self.dyhead = nn.Sequential(*[DyHeadBlock(ch[0]) for i in range(2)])
         c2, c3 = max((16, ch[0] // 4, self.reg_max * 4)), max(ch[0], min(self.nc, 100))  # channels
         self.cv2 = nn.ModuleList(
-            nn.Sequential(Conv(x, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch
+            nn.Sequential(Conv(x, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1,1)) for x in ch
         )
         self.cv3 = (
-            nn.ModuleList(nn.Sequential(Conv(x, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+            nn.ModuleList(nn.Sequential(Conv(x, c3, 3), nn.Conv2d(c3, self.nc, 1,1)) for x in ch)
             if self.legacy
             else nn.ModuleList(
                 nn.Sequential(
