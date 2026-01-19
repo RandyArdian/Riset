Fix IoU calculation by restoring original formula

ultralytics/utils/metrics.py
@@ -158,8 +158,8 @@ def bbox_iou(
     union = w1 * h1 + w2 * h2 - inter + eps
 
     # IoU
-    #iou = inter / union
-    iou = torch.pow(inter/(union + eps), alpha) 
+    iou = inter / union
+    #iou = torch.pow(inter/(union + eps), alpha) 
     if CIoU or DIoU or GIoU:
         cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
         ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height